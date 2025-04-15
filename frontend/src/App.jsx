a243d07d3fa09664aa3ecc84fba86f70629e4f5c
Your Name <you@example.com>
2025-04-15 17:18:45 +0200

    nevek_betöltése

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [receptek, setReceptek] = useState([]);
 
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/recipes/")
+      .then((valasz) => {
+        return valasz.json();
+      })
+      .then((receptek) => {
+        setReceptek(receptek);
+      }); //ez aszinkron módon fut le, nem sorba megy, csak akkor végzi el a műveletet, ha megkapja az adatot hozzá
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {receptek.map((recept) => (
+        <div>{recept.name}</div>
+      ))}
+    </div>
+  );
+};
 
-export default App
+export default App;
